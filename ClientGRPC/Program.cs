bc3b6a41c532c2bbcece9116175a978bb59b41e7
Bill Craun <bill@craun.net>
2022-01-05 06:52:45 -0800

    Initial commit

---
+++ ClientGRPC/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapr.Client;
+using System.Threading;
+
+namespace ClientGRPC
+{
+    class Program
+    {
+        class ResultMessage
+        {
+            public string Result { get; set; }
+        }
+
+        static async Task Main(string[] args)
+        {
+            var client = new DaprClientBuilder()
+                .Build();
+
+            var data = new { Name = "Reader" };
+
+            for(int x=0; x<10; x++)
+            {
+                var output = await client.InvokeMethodAsync<object, ResultMessage>("greetinggrpc", "SayHello", data);
+                Console.WriteLine(output.Result);            
+            }
+        }
+    }
+}
